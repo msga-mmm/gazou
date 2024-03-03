7af5c24b6e19621ef7fe9f83901955c44b9b0328
msga-mmm <michael.sga.mmm@gmail.com>
2024-03-03 01:32:16 -0500

    feat: add base logic to calculate etag

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,64 @@
 use deadpool_postgres::{
     tokio_postgres::NoTls, Config, Manager, ManagerConfig, Pool, RecyclingMethod, Runtime,
 };
-use ntex::web;
-
+use ntex::{
+    http::StatusCode,
+    web::{self, Responder},
+};
 #[web::get("/")]
+
 async fn hello() -> impl web::Responder {
     web::HttpResponse::Ok().body("Hello world!")
 }
 
 #[web::get("/images")]
-async fn images(pool: web::types::State<Pool>) -> impl web::Responder {
+async fn images(pool: web::types::State<Pool>, req: web::HttpRequest) -> web::HttpResponse {
     let client = pool.get().await.unwrap();
-    let statement = client
+
+    let images_etag_statement = client
+        .prepare_cached("SELECT md5(string_agg(id || name, '')) as etag FROM images")
+        .await
+        .unwrap();
+
+    let headers = req.headers();
+    let request_etag = headers.get("ETag");
+
+    let rows = client.query(&images_etag_statement, &[]).await.unwrap();
+    let response_etag = rows.get(0);
+
+	// TODO: move logic into middleware
+    if let Some(request_etag) = request_etag {
+        if let Some(response_etag) = response_etag {
+            let request_etag: String = request_etag.to_str().unwrap_or("").to_string();
+            let response_etag: String = response_etag.get(0);
+
+            if request_etag == response_etag {
+                return web::HttpResponse::NotModified().into();
+            }
+        }
+    }
+
+    let images_statement = client
         .prepare_cached("SELECT name FROM images")
         .await
         .unwrap();
-    let rows = client.query(&statement, &[]).await.unwrap();
+
+    let rows = client.query(&images_statement, &[]).await.unwrap();
 
     let mut response = String::new();
 
     for row in rows {
         response += row.get(0);
-		response += "\n";
+        response += "\n";
+    }
+
+    // web::HttpResponse::NotModified().body(response)
+
+    if let Some(response_etag) = response_etag {
+        let etag: String = response_etag.get(0);
+        return web::HttpResponse::Ok()
+            .set_header("ETag", etag)
+            .body(response);
     }
 
     web::HttpResponse::Ok().body(response)
